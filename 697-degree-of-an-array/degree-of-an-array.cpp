599619360f3f763b98aae6b279c5c8b6ceac9a9c
Abhishek <abhiofficial9504@gmail.com>
2026-08-11 23:44:30 +0530

    Time: 9 ms (70.93%) | Memory: 30.2 MB (19.23%) - LeetSync

---
+++ 697-degree-of-an-array/degree-of-an-array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        unordered_map<int, int> first;
+        unordered_map<int, int> last;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            freq[nums[i]]++;
+
+            if(first.find(nums[i]) == first.end())
+                first[nums[i]] = i;
+
+            last[nums[i]] = i;
+        }
+
+        int degree = 0;
+
+        for(auto it : freq) {
+            degree = max(degree, it.second);
+        }
+
+        int ans = nums.size();
+
+        for(auto it : freq) {
+
+            if(it.second == degree) {
+
+                int x = it.first;
+
+                int length = last[x] - first[x] + 1;
+
+                ans = min(ans, length);
+            }
+        }
+
+        return ans;
+    }
+};
