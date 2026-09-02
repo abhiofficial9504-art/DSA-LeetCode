0378a238663cb62b02713b307fdfaf95f31ebe83
Abhishek <abhiofficial9504@gmail.com>
2026-09-02 21:14:23 +0530

    Time: 15 ms (71.75%) | Memory: 12.7 MB (62.48%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -6,16 +6,20 @@ class Solution(object):
         """
         left = 0
         right = len(s) - 1
+        
         while left < right:
             if not s[left].isalnum():
                 left += 1
-                continue 
+                continue
+
             if not s[right].isalnum():
                 right -= 1
-                continue 
+                continue
+
             if s[left].lower() == s[right].lower():
                 left += 1
                 right -= 1
             else:
                 return False
+
         return True
